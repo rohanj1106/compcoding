2c79567e535e7417dc153051fa71c91383884014
Rohan Jain <rohajai@amazon.com>
2020-09-20 18:59:54 +0530

    initial commit

---
+++ abcd.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+#define ll long long int
+int main() {
+    ifstream cin("input.txt");
+    ll t,n,m,i,j,k,l,x,y,c;
+    vector<ll> arr;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        vector<vector<ll>> g(n);
+        for(i=0;i<n-1;i++){
+            cin>>x>>y;
+            
+        }
+    }
+
+	return 0;
+}
