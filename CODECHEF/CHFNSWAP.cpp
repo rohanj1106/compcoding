3fc7f8fe85e99ccf900ee4384c339e55348dc06d
Rohan Jain <rohajai@amazon.com>
2020-10-11 21:53:26 +0530

    CHFNSWAP started

--- CODECHEF/CHFNSWAP.cpp
+++ CODECHEF/CHFNSWAP.cpp
@@ -1,42 +1,27 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <math.h>
 using namespace std;
 #define ll long long int
 
-ll natSum(ll n){
+ll sumOfNaturalNumbers(ll n){
     return ((n*(n+1))/2);
 }
-
-ll partition(ll n){
-    ll sum = natSum(n);
-    if(sum%2==1){
-        return -1;
-    }
-    ll l=1,h=n;
-    while(l<=h){
-        ll mid=l+(((h-l))/2);
-        ll diff = (sum/2)-natSum(mid);
-        cout<<mid<<" "<<diff<<" "<<natSum(mid)<<" "<<sum<<endl;
-        if(diff>=1&&diff<=(n-1)){
-            return mid;
-        }else if(diff>(n-1)){
-            l=mid+1;
-        }else{
-            h=mid-1;
-        }
-    }
-    return -1;
+ll getHalfSumPoint(ll n){
+    ll sum = sumOfNaturalNumbers(n);
+    double D = sqrt(1.0+(4.0)*(double)(sum));
+    return ((D-1)/2);
 }
 int main()
 {
     ifstream cin("input.txt");
     ll t, n, m, i, j, k, l, x, y, c;
     cin>>t;
-    //cout<<t<<endl;
     while (t--)
     {
         cin>>n;
-        cout<<partition(n)<<endl;
+        ll halfSumPoint = getHalfSumPoint(n);
+        cout<<halfSumPoint<<" "<<sumOfNaturalNumbers(halfSumPoint)<<" "<<sumOfNaturalNumbers(n)<<endl;
     }
 }
